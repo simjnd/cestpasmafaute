e5c419257788d424e67335ceb590f11e22aa74d9
Climent <arnaud.climent@orange.fr>
2019-11-14 16:26:29 +0100

    Ajout difficulty

---
+++ models/Difficulty.php
@@ -0,0 +1,23 @@
+<?php
+namespace CPMF\Models;
+class Difficulty
+{
+	private $id;
+	private $label;
+
+	public function __construct($id, $label)
+	{
+		$this->id = $id;
+		$this->label = $label;
+	}
+
+	public function getId(): int
+	{
+		return $this->id;
+	}
+
+	public function getLabel(): string
+	{
+		return $this->label;
+	}
+}
