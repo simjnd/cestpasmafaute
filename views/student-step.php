10b7651eb462d2ba6a1e43ebbed229de60093e66
Climent <arnaud.climent@orange.fr>
2019-12-05 15:43:55 +0100

    Fin de student-step et student-question-clickable

---
+++ views/student-step.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1><?= $stepTitle ?></h1>
+	<img src="<?= $avatar ?>">
+	<p><?= $studentClass ?></p>
+	<p><?= $studentTotalPoint ?></p>
+	<p><?= $currentLesson ?></p>
+	<p>Exercice facile</p>
+	<p>Exercice intermediate</p>
+	<p>Exercice expert</p>
+
+	<a href="<?= $pathCurrentExercise ?>">Faire l'exercice</a>
+</body>
+</html>
