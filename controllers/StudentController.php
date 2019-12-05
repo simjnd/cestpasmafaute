9a26c12d914766a6f793c471d97ff227d747a604
Simon Janda <janda.simon@icloud.com>
2019-12-05 16:04:57 +0100

    Update StudentController.php

--- controllers/StudentController.php
+++ controllers/StudentController.php
@@ -3,5 +3,8 @@ namespace CPMF\Controller;
 
 class StudentController extends Controller
 {
-    
+    public function seeProfile(int $id): void
+    {   
+        parent::view('student-see-class');
+    }
 }
