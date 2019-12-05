63ca3dc2839da0e6595003914d5f82d204ce4d98
voydstack <voydstack@gmail.com>
2019-12-05 18:12:23 +0100

    StudentController fix

--- controllers/StudentController.php
+++ controllers/StudentController.php
@@ -17,6 +17,6 @@ class StudentController extends Controller
     public function seeHomepage(): void
     {
         $steps = StepManager::getStepsByStudentID($_SESSION['idLogin']);
-        $student = 
+        //$student = 
     }
 }
