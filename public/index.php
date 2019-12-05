017d9cc7945bbca921a4c03afddafbd8798f676e
Simon Janda <janda.simon@icloud.com>
2019-12-05 13:55:45 +0100

    Routessss

--- public/index.php
+++ public/index.php
@@ -24,9 +24,26 @@ Router::init();
 
 Router::setDefault('NotFound');
 
+// GLOBAL PAGES
+
+Router::get('/signout', 'Global@signOut');
+
 Router::view('/', 'hello');
 Router::view('/login', 'login');
 Router::post('/login', 'Login@postLogin');
 Router::get('/connected', 'Connected@connectedPage');
+
+// STUDENT PAGES
+
+Router::get('/class', 'Student@seeClass');
+
+Router::get('/profile', 'Student@seeProfile');
+Router::post('/profile', 'Student@saveProfileChanges');
+
+// TEACHER PAGES
+
+Router::get('/approval', 'Teacher@seeWaitingStudents');
+Router::get('/approval', 'Teacher@seeWaitingStudents');
+
 
 Router::run();
