37c081fecefcd4b6aae868ca61fb8ffac5fcb366
Arnaud Climent <arnaud.climent@etu.iut-tlse3.fr>
2019-12-19 15:31:16 +0100

    Implementation seeWaintingStudents

--- models/entities/Teacher.php
+++ models/entities/Teacher.php
@@ -3,9 +3,6 @@ namespace CPMF\Models\Entities;
 
 class Teacher extends User 
 {
-	private $email;
-	private $firstName;
-	private $lastName;
 	private $classes;
 
 	public function __construct(array $data)
@@ -18,21 +15,6 @@ class Teacher extends User
         if(method_exists($this, $methodName)) {
             $this->$methodName($value);
         }   
-    }
-
-    private function setEmail(string $email): void
-    {
-    	$this->email = $email; 
-    }
-
-    private function setFirstName(string $firstName): void
-    {
-    	$this->firstName = $firstName;
-    }
-
-    private function setLastName(string $lastName): void
-    {
-    	$this->lastName = $lastName;
     }
 
     private function setClasses(array $classes): void
