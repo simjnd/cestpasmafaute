bb93a49754323c61aa77a05877ccc81b2e2a7248
Climent <arnaud.climent@orange.fr>
2019-12-23 15:43:16 +0100

    Gestion des mots de passe oublié

--- views/general-forgot-password.php
+++ views/general-forgot-password.php
@@ -6,7 +6,7 @@
 <body>
 	<h1>Mot de passe oublié</h1>
 	<p>Entrer votre email pour changer votre mot de passe</p>
-	<form method="/forgot-password" action="POST">
+	<form action="/forgot-password" method="POST">
 		<label for="email">Adresse E-mail: </label>
 		<input type="email" name="email">
 		<input type="submit" value="Envoyer">
