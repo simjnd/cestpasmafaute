390d4bed2097b5ced7d79371e9bd4f09887a3c13
Climent <arnaud.climent@orange.fr>
2019-12-23 17:58:22 +0100

    Gestion mots de passe oublié

--- controllers/GeneralController.php
+++ controllers/GeneralController.php
@@ -94,7 +94,7 @@ class GeneralController extends Controller
 		if ($userExists) {
 			$idLogin = UserManager::getIdByEmail($email);
 
-			$token = random_bytes(32);
+			$token = bin2hex(random_bytes(32));
 			var_dump($token);
 
 			UserManager::addToken($idLogin, $token);
