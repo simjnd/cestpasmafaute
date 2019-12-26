35cf4955c998be40893df62726fffff7b40f3ce9
Climent <arnaud.climent@orange.fr>
2019-12-26 18:46:35 +0100

    Implementation changeForgotPassword

--- public/index.php
+++ public/index.php
@@ -45,7 +45,7 @@ Router::post('/forgot-password', 'General@sendPasswordEmail');
 
 Router::view('/email-sended', 'general-email-sended');
 
-Router::get('/change-forgot-password/{idlogin}/{token}', 'general-change-forgot-password');
+Router::view('/change-forgot-password/{idlogin}/{token}', 'general-change-forgot-password');
 Router::post('/change-forgot-password/{idLogin}/{token]', 'General@changeForgotPassword');
 
 
