53d2fbe2e92c0abf0fb9fcaff663a70ef70218d6
DragnirMK <valentin.pierre82@gmail.com>
2019-12-27 13:03:56 +0100

    see-step lessons3

--- views/student-step.php
+++ views/student-step.php
@@ -60,17 +60,17 @@
                 case 'easy':
                     path.setAttribute('href', currentLink + '/exercice/0');
                     path.innerHTML = "Faire l'exercice facile";
-                    lesson.innerHTML = <?= echo json_encode($lessons[0], JSON_HEX_TAG); ?>;
+                    lesson.innerHTML = <?= $lessons[0] ?>;
                     break;
                 case 'medium':
                     path.setAttribute('href', currentLink + '/exercice/1');
                     path.innerHTML = "Faire l'exercice intermediaire";
-                    lesson.innerHTML = <?= echo json_encode($lessons[1], JSON_HEX_TAG); ?>;
+                    lesson.innerHTML = <?= $lessons[1] ?>;
                     break;
                 case 'hard':
                     path.setAttribute('href', currentLink + '/exercice/2');
                     path.innerHTML = "Faire l'exercice avancé";
-                    lesson.innerHTML = <?= echo json_encode($lessons[2], JSON_HEX_TAG); ?>;
+                    lesson.innerHTML = <?= $lessons[2] ?>;
                     break;
                 default:
                     break;
