c08c07adad0af35be771c61abb55310fddc0b3c9
DragnirMK <valentin.pierre82@gmail.com>
2019-12-27 11:35:48 +0100

    issue with query3

--- controllers/StudentController.php
+++ controllers/StudentController.php
@@ -43,6 +43,8 @@ class StudentController extends Controller
     {
         $student = StudentManager::getById($_SESSION['idLogin']);
         $step = StudentExerciceManager::getStepByID($id);
+        echo $step->getIdStep();
+        echo $step->getName();
         $step->fill();
         $lessons = $step->getLessons();
         $totalPoints = StudentManager::getTotalPoints($_SESSION['idLogin']);
