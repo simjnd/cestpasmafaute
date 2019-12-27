85574b02af8d91adc173858514d81b53f0da25a1
DragnirMK <valentin.pierre82@gmail.com>
2019-12-27 12:50:18 +0100

    see-step fix url change with javascript2

--- views/student-step.php
+++ views/student-step.php
@@ -58,17 +58,17 @@
             let id = this.id;
             switch (id) {
                 case 'easy':
-                    path.setAttribute('href', '/exercice/0');
+                    path.setAttribute('href', currentLink + '/exercice/0');
                     path.innerHTML = "Faire l'exercice facile"
                     lesson.innerHTML = "test";
                     break;
                 case 'medium':
-                    path.setAttribute('href', '/exercice/1');
+                    path.setAttribute('href', currentLink + '/exercice/1');
                     path.innerHTML = "Faire l'exercice intermediaire"
                     lesson.innerHTML = "test2";
                     break;
                 case 'hard':
-                    path.setAttribute('href', '/exercice/2');
+                    path.setAttribute('href', currentLink + '/exercice/2');
                     path.innerHTML = "Faire l'exercice avancé"
                     lesson.innerHTML = "test3";
                     break;
