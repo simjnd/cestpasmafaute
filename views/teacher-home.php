ac076334ebf09da806df62f661b05e4f3dffb76c
voydstack <voydstack@gmail.com>
2020-01-05 23:37:13 +0100

    Améliorations de la vue teacher-home

--- views/teacher-home.php
+++ views/teacher-home.php
@@ -10,29 +10,22 @@
 		<img src="#logoDuSite">
 		<h1> Tableau de bord </h1>
 		<img src="#profil">
-		<p> <?php $teacher->getFirstName(); 
-		$teacher->getLastName(); ?> 
-	</p>
-</header>
+		<p><?= $teacher->getFirstName().' '.$teacher->getLastName() ?></p> 
+	</header>
 
-<div>
-	<a href="#attenteValidation"> <?php $numberWaitingStudent ?> en attente de validation </a>
-</div> <br>
+	<div>
+		<a href="/approval"> <?php $numberWaitingStudent ?> élèves en attente de validation </a>
+	</div> <br>
 
-<table>
-	<tr>Mes classes </tr>
-	<div> Ajouter </div>
-	<?php foreach ($classes as $class) { ?>
-		<tr>
-			<p>
-				<?= $class->getName() ?>
-			</p>
-			<p>
-				<?= count($class->getStudents()) ?>
-			</p>
-			<a href="modifierClasse"></a>
-		</tr>
-	<?php } ?>
-</table>
+	<h1>Mes classes</h1>
+	<table>
+		<?php foreach ($classes as $class) { ?>
+			<tr>
+				<td><a href="/class/<?= $class->getIdClass() ?>"><?= $class->getName() ?></a></td>
+				<td><?= count($class->getStudents()) ?> élèves</td>
+				<td><a href="/class/<?= $class->getIdClass() ?>/edit">Modifier</a></td>
+			</tr>
+		<?php } ?>
+	</table>
 </body>
 </html>
