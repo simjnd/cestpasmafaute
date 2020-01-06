342df849cd410e82f22cb9b68655f77bd39b7f15
Simon Janda <janda.simon@icloud.com>
2020-01-06 12:13:20 +0100

    Update StudentExerciseManager.php

--- models/StudentExerciseManager.php
+++ models/StudentExerciseManager.php
@@ -18,7 +18,7 @@ class StudentExerciseManager
 	{
     	$steps = [];
     	
-    	$query = Manager::getDatabase()->prepare('SELECT Step.idStep AS idStep, Step.name AS name FROM Login, Student, Class, Course_Step, Step WHERE Login.idLogin = :idLogin AND Login.idLogin = Student.idLogin AND Student.idClass = Class.idClass AND Class.idCourse = Course_Step.idCourse AND Course_Step.idStep = Step.idStep');
+    	$query = Manager::getDatabase()->prepare('SELECT Step.idStep AS idStep, Step.name AS name, Step.color AS color, Step.image as image FROM Login, Student, Class, Course_Step, Step WHERE Login.idLogin = :idLogin AND Login.idLogin = Student.idLogin AND Student.idClass = Class.idClass AND Class.idCourse = Course_Step.idCourse AND Course_Step.idStep = Step.idStep');
     	$query->execute(['idLogin' => $idLogin]);
     	
     	foreach ($query->fetchAll() as $rawStep) {
