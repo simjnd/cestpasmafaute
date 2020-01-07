5ca6f59dd584c3b5a4bd391f41ebefc54f9da653
voydstack <voydstack@gmail.com>
2020-01-07 19:59:08 +0100

    Ajout d'éléments manquants dans la vue

--- views/student-exercises.php
+++ views/student-exercises.php
@@ -7,20 +7,38 @@
     <link rel="stylesheet" href="assets/css/green-scheme.css">
 </head>
 <body>
+	<header>
+        <div id="logo">
+            <a href="#">
+                <picture>
+                    <source media="(max-width: 1024px)" srcset="assets/img/logo-horizontal-small.svg">
+                    <img src="assets/img/logo-horizontal.svg">
+                </picture>
+            </a>
+        </div>
+        <div id="title">
+            <p><span>300</span> points</p>
+            <progress value="50" max="100"></progress>
+        </div>
+        <div id="profile">
+            <a href="#">
+                <div></div>
+            </a>
+        </div>
+    </header>
 	<section id="content">
         <p id="step-name">Nom de l'étape</p>
         <h1 id="question-number">Question <span class="id-question"></span></h1>
+        <p>Explication sur comment répondre à ce type de question</p>
         <div id="question">
-   			<p>Explication sur comment répondre à ce type de question</p>
-            <div id="question-content">
-                <p class="sentence"></p>
-            </div>
-            <a href="#">
-                <div class="answer">
-                    <p>Valider</p>
-                </div>
-            </a>
+   			<p class="sentence"></p>
+            <div id="question-content"></div>
         </div>
+        <a href="#">
+            <div class="answer">
+                <p>Valider</p>
+            </div>
+        </a>
     </section>
 	<footer>
 	</footer>
